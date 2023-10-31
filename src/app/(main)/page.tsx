4b28a7ea09a5b1f2e004ca2aff5d27d9550d3592
leo <leo.wei.badger@gmail.com>
2023-10-31 10:13:44 +0800

    增加在线聊天功能

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,6 +1,7 @@
 import FeaturedProducts from "@modules/home/components/featured-products"
 import Hero from "@modules/home/components/hero"
 import { Metadata } from "next"
+import Script from "next/script"
 
 export const metadata: Metadata = {
   title: "Home",
@@ -11,6 +12,18 @@ export const metadata: Metadata = {
 const Home = () => {
   return (
     <>
+     <Script id="rocket-chat" strategy="afterInteractive">
+                {`
+                    // paste the javascript code here
+                    // the immediately invoked function is just an example
+                    (function(w, d, s, u) {
+                      w.RocketChat = function(c) { w.RocketChat._.push(c) }; w.RocketChat._ = []; w.RocketChat.url = u;
+                      var h = d.getElementsByTagName(s)[0], j = d.createElement(s);
+                      j.async = true; j.src = 'https://medusa.rocket.chat/livechat/rocketchat-livechat.min.js?_=201903270000';
+                      h.parentNode.insertBefore(j, h);
+                    })(window, document, 'script', 'https://medusa.rocket.chat/livechat');
+                `}
+            </Script>
       <Hero />
       <FeaturedProducts />
     </>
